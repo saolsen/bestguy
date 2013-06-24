f426ca31a9026c63d6a1bb4b960528848f812c17
Stephen Olsen <steve.olsen@gmail.com>
2013-06-24 18:15:38 -0400

    ring app for example app

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Library for declaritive creation of 3d models (and creatures!)
 
+Also includes example app which is a cljs/clj web app that you can use to play around with the library.
+
 ## License
 
 Copyright © 2013 FIXME
--- examples/clj/bestguy_examples/app.clj
+++ examples/clj/bestguy_examples/app.clj
@@ -1,4 +1,16 @@
 (ns bestguy-examples.app
+  (:require [compojure.core :refer :all]
+            [ring.adapter.jetty :as jetty])
   (:gen-class))
 
-(defn -main [& args] (println "hello world"))
+(defroutes app
+  (GET "/" r "bestguy example app"))
+
+(defn create-app [port]
+  (jetty/run-jetty app {:join? false :port port}))
+
+(defn -main
+  [& args]
+  (let [port (Integer/parseInt (first args))
+        server (create-app port)]
+    (def server server)))
