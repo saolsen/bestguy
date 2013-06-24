bb5d350e5d2984e38dc94cc58f63acab8aa2f5d0
Stephen Olsen <steve.olsen@gmail.com>
2013-06-24 19:31:48 -0400

    js loaded in endpoint

--- examples/clj/bestguy_examples/app.clj
+++ examples/clj/bestguy_examples/app.clj
@@ -1,10 +1,24 @@
 (ns bestguy-examples.app
   (:require [compojure.core :refer :all]
-            [ring.adapter.jetty :as jetty])
+            [compojure.route :as route]
+            [ring.adapter.jetty :as jetty]
+            [hiccup.core :as h])
   (:gen-class))
 
 (defroutes app
-  (GET "/" r "bestguy example app"))
+  (GET "/" r
+       (str "<!DOCTYPE html>"
+            (h/html [:html
+                     [:head
+                      [:title "bestguy-example"]]
+                     [:body
+                      "hello world buddy"
+                      [:script {:src "js/three.min.js"}]
+                      [:script {:src "js/example.js"}]
+                      ]
+                     ])))
+  (route/resources "/")
+  (route/not-found "page not found"))
 
 (defn create-app [port]
   (jetty/run-jetty #'app {:join? false :port port}))
--- examples/cljs/bestguy_examples/main.cljs
+++ examples/cljs/bestguy_examples/main.cljs
@@ -1,2 +1,11 @@
 (ns bestguy-examples.main)
+;; Depends on some js libraries.
+;; three.js
+;; 
+
+;; Also does very chromey things for now and
+;; probably won't work in other browsers.
+
+
+
 (js/alert "hello world")
