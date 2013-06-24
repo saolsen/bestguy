ad17950b052d18d85a674c95977f42ed18fc155c
Stephen Olsen <steve.olsen@gmail.com>
2013-06-24 10:35:18 -0400

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# bestguy
+
+Library for declaritive creation of 3d models (and creatures!)
+
+## License
+
+Copyright © 2013 FIXME
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject bestguy "0.1.0-SNAPSHOT"
+  :description "Library for declaritive creation of 3d models and creatures"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
