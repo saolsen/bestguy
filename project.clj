a8485a1f4cbf8732bdc021201a2cf6ae3be960ae
Stephen Olsen <steve.olsen@gmail.com>
2013-06-24 17:11:01 -0400

    project setup

--- project.clj
+++ project.clj
@@ -3,4 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {:examples {:plugins [[lein-cljsbuild "0.3.0"]]
+                        :source-paths ["examples/clj"]
+                        :cljsbuild
+                        {:builds
+                         [{:id "examples"
+                           :source-paths ["examples/cljs"]
+                           :compiler {:optomizations :simple
+                                      :output-to "resources/js/example.js"}}]}}})
